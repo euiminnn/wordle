47763d79a1a87f406252f8981423b1808521ddcc
Euimin Chung <jane3079@korea.ac.kr>
2022-02-07 16:09:12 +0900

    chore: edit PAGE INFO

--- src/App.tsx
+++ src/App.tsx
@@ -98,7 +98,7 @@ function App() {
 
   return (
     <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <p>AI Meets Everything, <a href='https://clova.ai'>CLOVA</a></p>
+      <p>모든 42서울인을 위한 커뮤니티, <a href='https://www.42world.kr/'>🌎 42world</a></p>
       <div className="flex w-80 mx-auto items-center mb-8">
         <h1 className="text-xl grow font-bold">Wordle (한글 풀어쓰기 5자)</h1>
         <InformationCircleIcon
@@ -148,7 +148,7 @@ function App() {
         className="mx-auto mt-8 flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 select-none"
         onClick={() => setIsAboutModalOpen(true)}
       >
-        이 게임에 대하여
+        페이지 정보
       </button>
 
       <Alert message="Not enough letters" isOpen={isNotEnoughLetters} />
